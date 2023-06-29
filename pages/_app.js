6a801490f474888a0d23e3b623495eb3b8eb5999
Chirag Patel <chirag.patel@bytestechnolab.com>
2023-06-29 23:26:48 +0530

    ui corrections

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Ek Ped Ek Zindgi</title>
+      <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="/images/favicon.ico" type="image/x-icon"></link>
+    </Head> <Component {...pageProps} />
+  </>
 }
